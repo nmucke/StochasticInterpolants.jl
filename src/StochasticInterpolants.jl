71b76c517c563ae8ec10aec124094806057f28d1
ntm <nmucke@gmail.com>
2024-04-26 16:58:03 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.49

---
+++ src/StochasticInterpolants.jl
@@ -0,0 +1,5 @@
+module StochasticInterpolants
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using StochasticInterpolants
+using Test
+
+@testset "StochasticInterpolants.jl" begin
+    # Write your tests here.
+end
